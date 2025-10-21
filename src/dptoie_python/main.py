9b9293845047e0270ebb226e96c907dc2f304a81
André Walker <andre.walkermo@gmail.com>
2025-10-21 00:59:50 -0300

    refactor: move arquivos para buildar lib

--- src/dptoie_python/main.py
+++ src/dptoie_python/main.py
@@ -14,7 +14,7 @@ from spacy.tokens import Doc
 from spacy import Language
 from typing import Any, Generator
 from spacy_conll.parser import ConllParser
-from src.extraction import Extractor, ExtractorConfig, Extraction
+from src.dptoie_python.extraction import Extractor, ExtractorConfig, Extraction
 
 
 def generate_conll_file_from_sentences_file(input_file: str) -> str:
